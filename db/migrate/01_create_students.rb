3fa8eb416b294accbec4631aec18088eaa0e1ab8
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-05 17:58:45 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -4,8 +4,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     create_table :students do |t|
       t.string :name
       t.string :grade
-      t.integer :birthdate
-      t.string :hometown
     end
   end
 end
